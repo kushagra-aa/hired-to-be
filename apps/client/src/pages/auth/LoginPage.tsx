d055398ae2e4dfdb1d24eb36b173cbef197d21c1
kushagra-aa <kushagraagnihotri24@gmail.com>
2025-09-28 20:47:56 +0530

    Feature: FE - Login Page

--- apps/client/src/pages/auth/LoginPage.tsx
+++ apps/client/src/pages/auth/LoginPage.tsx
@@ -1,102 +1,38 @@
-import { ApiError } from "@hiredtobe/shared/lib";
-import { LoginFormSchema, LoginFormType } from "@hiredtobe/shared/schemas";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { Link, useLocation, useNavigate } from "react-router";
-
-import { USER_LANDING_PAGES } from "@/client/app/router";
-import UIButton from "@/client/components/ui/Button";
-import UIFormWrapper from "@/client/components/ui/FormWrapper";
+import { useTheme } from "@/client/app/ThemeProvider";
+import GoogleSigninButton from "@/client/components/GoogleSigninButton";
 import {
-  FormControlType,
-  UIInputField,
-} from "@/client/components/ui/InputField";
-import Loader from "@/client/components/ui/Loader";
-import { makeToast } from "@/client/components/ui/Toast";
-import { useLogin } from "@/client/hooks/useAuthActions";
-import { Form } from "@/client/shadcn/components/ui/form";
-import { useAuth } from "@/client/stores/auth.store";
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/client/shadcn/components/ui/card";
 
 export default function LoginPage() {
-  const form = useForm<LoginFormType>({
-    resolver: zodResolver(LoginFormSchema),
-  });
-
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  const login = useLogin();
-  const { user } = useAuth();
-
-  // If user was redirected from a protected route, go back there
-
-  const handleFormSubmit = (data: LoginFormType) => {
-    login.mutate(
-      { email: data.email, googleID: data.googleID },
-      {
-        onSuccess: async () => {
-          const from =
-            (location.state as { from?: Location })?.from?.pathname ||
-            USER_LANDING_PAGES[user!.role];
-          makeToast.success("Logged in successfully");
-          await navigate(from, { replace: true });
-        },
-        onError: (err: ApiError) => {
-          form.setError("root", { message: err.data?.message || err.message });
-        },
-      },
-    );
-  };
+  const theme = useTheme();
 
   return (
-    <main className="w-[100vw] min-h-[100vh] p-10 flex flex-col items-center justify-center">
-      <h1 className="text-4xl">🔑 Login</h1>
-      <Form {...form}>
-        <UIFormWrapper
-          error={form.formState.errors.root?.message}
-          className="space-y-2 w-11/12 sm:w-3/4 md:w-2/3 lg:w-1/2"
-          onSubmit={form.handleSubmit(handleFormSubmit)}
-        >
-          <UIInputField
-            {...form.register("email")}
-            control={form.control as unknown as FormControlType}
-            label="Email"
-            type="email"
-            placeholder="Enter Email"
-            icon={"X"}
-            error={form.formState.errors?.email?.message}
-          />
-          <UIInputField
-            {...form.register("googleID")} // Handles 'name', 'onChange', 'value'
-            control={form.control as unknown as FormControlType}
-            error={form.formState.errors?.googleID?.message}
-            as="password"
-            label="Google ID"
-            placeholder="Enter Google ID"
-          />
-          <UIButton
-            className="min-w-1/4"
-            type="submit"
-            disabled={login.isPending}
-          >
-            {login.isPending ? (
-              <Loader variant="clip" size={"xs"} color="secondary" />
-            ) : (
-              "Login"
-            )}
-          </UIButton>
-        </UIFormWrapper>
-      </Form>
-      <Link
-        style={{
-          display: "inline-block",
-          marginTop: "1rem",
-          color: "var(--color-primary)",
-        }}
-        to="/register"
-      >
-        No Account? Register!
-      </Link>
+    <main className="min-h-[100vh] py-20 flex flex-col items-center justify-start gap-16">
+      <h1 className="text-4xl">
+        <span className="no_underline">🔑</span>
+        <span className="underline underline-offset-8 decoration-primary">
+          Login
+        </span>
+      </h1>
+      <div className="flex-1 h-full w-full flex items-center justify-center">
+        {/* Card */}
+        <Card className="w-[70%] md:w-[65%] lg:w-[50%] xl:w-[40%] py-10 min-h-[50vh] items-center justify-start gap-0">
+          <CardHeader className="w-full flex flex-col items-center gap-4">
+            <CardTitle className="w-max">Login || Signup</CardTitle>
+            <CardDescription className="w-max">
+              Use Google Login
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="w-full h-full flex-1 flex items-center justify-center">
+            <GoogleSigninButton theme={theme.theme} />
+          </CardContent>
+        </Card>
+      </div>
     </main>
   );
 }
